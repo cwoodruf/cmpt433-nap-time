344df3759bcf789df69bad2e7bfc9d8bd7afbb87
cwoodruf@gmail.com <cwoodruf@gmail.com@b7c43c20-ba61-c9b8-7786-de23efc508d3>
2011-11-05 08:12:25 +0000

    made napbridge tools for website

--- trunk/napmedia/controllers/home.php
+++ trunk/napmedia/controllers/home.php
@@ -17,6 +17,29 @@ class Home extends Controller {
 	 * required function for controllers
 	 */
 	public function execute() {
+		$this->doable(array(
+			'restartbridge' => 'restartbridge',
+			'default' => 'form',
+		));
+		$this->doaction($this->actions[1]);
+	}
+	protected function restartbridge() {
+		exec("killall napbridge.pl");
+		exec("echo `/bin/date` >> napbridge.txt");
+		exec("naptime/napbridge.pl -v >> napbridge.txt 2>&1 &",$outarray,$response);
+		View::assign('topmsg','napbridge.pl restarted');
+		$this->form();
+	}
+	protected function bridgestatus() {
+		$cwd = getcwd();
+		$pid = trim(file_get_contents("nodes/napbridge.pid"));
+		if ($pid and preg_match("#$pid.*napbridge.pl#",`ps -ef`)) {
+			return "napbridge.pl running on ".htmlentities($pid);
+		}
+		return "napbridge.pl is not running $pid";
+	}
+	protected function form() {
+		View::assign('bridgestatus',$this->bridgestatus());
 		View::assign('nodes',Nap::ipsstr());
 		View::assign('files',Nap::sumsstr());
 		View::wrap('home.tpl');
