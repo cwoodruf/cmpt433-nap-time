09500d88849d488b06bbf7b3e5f71d2c5c5f4a70
cwoodruf@gmail.com <cwoodruf@gmail.com@b7c43c20-ba61-c9b8-7786-de23efc508d3>
2011-11-06 11:10:39 +0000

    added some test scripts and data for napbridge

--- napmedia/naptime/napbridge.pl
+++ napmedia/naptime/napbridge.pl
@@ -1,37 +1,23 @@
 #!/usr/bin/perl
 # purpose of this script is to listen for connections on our listening port and
 # try and send messages between napmedia client boards
-# see http://www.thegeekstuff.com/2010/07/perl-tcp-udp-socket-programming/ for socket original
+# see http://www.thegeekstuff.com/2010/07/perl-tcp-udp-socket-programming/ for inet socket original
+# see http://www.rexroof.com/blog/2005/09/unix-domain-sockets-in-perl.php for unix socket example
 
-use POSIX qw/mkfifo/;
-use Fcntl qw/:flock/;
+use Nap;
 use IO::File;
 use IO::Select;
 use IO::Socket::UNIX;
 use IO::Socket::INET;
-use PHP::Serialization qw/serialize unserialize/;
 use Getopt::Std;
 use Data::Dumper;
 
 use strict;
 $| = 1; # autoflush
 
-###################### types of requests ######################################
-my %reqtype = (
-	'peers' => \&peers, # list of currently alive ips
-	'index' => \&getindex, # needs a neighbour ip
-	'file' => \&getfile, # needs neighbour ip and file id
-	'stream' => \&stream, # sends continuous stream of bytes
-);
-
 ###################### process input ##########################################
-my $defport = 29533;
-my $defnodedir = '/usr/local/apache2/htdocs/napmedia/nodes';
-my $defnodelife = '60';
-my $nodelifemin = '1';
-my $nodelifemax = '86400';
-my %opt;
-getopts('p:d:hv',\%opt);
+our %opt;
+getopts('H:p:d:hv',\%opt);
 my $usage = <<TXT;
 $0 [-p{port}] [-d{node dir}] [-t{ttl}] [-v] [-h]
 
@@ -39,28 +25,28 @@ To run:
 $0 [options] >> {logfile} 2>&1 &
 
 Options:
-	-p{port} port to listen on for requests ($defport)
-	-d{dir} where to find php serialized node data ($defnodedir)
-	-t{ttl} time to live in seconds for a node entry ($defnodelife)
+	-p{port} port to listen on for requests ($napport)
+	-d{dir} where to find php serialized node data ($napnodedir)
+	-t{ttl} time to live in seconds for a node entry ($napnodelife)
 	-v verbose output
 	-h this help
 TXT
 print $usage and exit if $opt{h};
 
-my $port = $opt{p} || $defport;
+my $port = $opt{p} || $napport;
 die "port should be a number between 1024 and 65535!\n$usage" 
 	unless $port >= 1024 and $port <= 65535;
 
-my $nodedir = $opt{d} || $defnodedir;
-die "need a valid node directory!\n$usage" unless -d $nodedir;
+$Nap::nodedir = $opt{d} || $napnodedir;
+die "need a valid node directory!\n$usage" unless -d $Nap::nodedir;
 
-my $nodelife = $opt{t} || $defnodelife;
-die "node time to live should be between $nodelifemin and $nodelifemax!"
-	unless $nodelife >= $nodelifemin and $nodelife <= $nodelifemax;
+$Nap::nodelife = $opt{t} || $napnodelife;
+die "node time to live should be between $napnodelifemin and $napnodelifemax!"
+	unless $napnodelife >= $napnodelifemin and $napnodelife <= $napnodelifemax;
 
 if ($opt{v}) {
-	print "listening on $port\nnode directory $nodedir\nnode ttl $nodelife s\n";
-	print "allowed commands:\n\t", join "\n\t", keys %reqtype;
+	print "listening on $port\nnode directory $Nap::nodedir\nnode ttl $napnodelife s\n";
+	print "allowed commands:\n\t", join "\n\t", keys %napreq;
 	print "\n";
 }
 
@@ -77,10 +63,10 @@ my $buff;
 my $buffsize = 8 * 1024;
 $SIG{CHLD} = 'IGNORE'; # automatically reap spent worker processes
 
-open PIDFILE, "> $nodedir/napbridge.pid" or die "can't save pid in $nodedir/naptime.pid: $!";
+open PIDFILE, "> $Nap::nodedir/napbridge.pid" or die "can't save pid in $Nap::nodedir/naptime.pid: $!";
 print PIDFILE $$;
 close PIDFILE;
-print "saved pid $$ to $nodedir/napbridge.pid\n" if $opt{v};
+print "saved pid $$ to $Nap::nodedir/napbridge.pid\n" if $opt{v};
 
 while (1) {
 	my $clientsock = $listener->accept;
@@ -89,7 +75,8 @@ while (1) {
 		print "started worker $pid\n" if $opt{v};
 	} else {
 		# keep track of all the other nodes that have connected to you 
-		exit 1 unless (my $fifo = &save_connection($clientsock));
+		exit 1 unless (my $node = &save_connection($clientsock));
+		my $fifo = $node->{fifoconn};
 		my $sel = IO::Select->new();
 		$sel->add($clientsock);
 		$sel->add($fifo);
@@ -105,7 +92,7 @@ while (1) {
 							print $clientsock "FAIL\n";
 						}
 					} else {
-						print "ACKing\n" if $opt{v};
+						print "ACKing $node->{ip}\n" if $opt{v};
 						print $clientsock "ACK\n";
 					}
 				} elsif ($reader == $fifo) {
@@ -121,7 +108,6 @@ while (1) {
 					}
 					print $fifoconn $cbuff;
 					$fifoconn->flush;
-					# close $fifoconn;
 				}
 			}
 		}
@@ -134,11 +120,12 @@ sub save_connection {
 	my ($socket) = @_;
 	my $ip = $socket->peerhost;
 	my $node = &nodeload($ip);
-	my $fifo = "$nodedir/$ip.sock";
+	my $fifo = "$Nap::nodedir/$ip.sock";
 	$node->{fifo} = $fifo;
 	$node->{ip} = $ip;
 	$node->{port} = $socket->peerport;
 	$node->{ts} = time;
+	# this unix socket is meant to live for the life of the process
 	if (&nodesave($ip,$node)) {
 		unlink $fifo;
 		my $local = IO::Socket::UNIX->new(
@@ -146,48 +133,11 @@ sub save_connection {
 			Type => SOCK_STREAM,
 			Listen => 10,
 		) or warn "can't open local socket: $!" and return;
-		return $local;
+		# pointless to save this to file - so its set here
+		$node->{fifoconn} = $local;
+		return $node;
 	}
 	return;
-}
-
-sub valid_ip {
-	my ($ip) = @_;
-	$ip =~ /((?:\d{1,3}\.){3}\d{1,3})/;
-	return $1;
-}
-
-sub nodeload {
-	my ($ip) = @_;
-	warn "nodeload: $ip wrong form - aborting!" and return 
-		unless $ip = &valid_ip($ip);
-	my $node;
-	local $/ = undef;
-	if (open NODE, "$nodedir/$ip.php") {
-		flock NODE,LOCK_SH;
-		$node = unserialize(<NODE>);
-		flock NODE,LOCK_UN;
-		close NODE;
-	}
-	$node;
-}
-
-sub nodesave {
-	my ($ip, $node) = @_;
-	warn "nodesave: $ip wrong form - aborting!" and return 
-		unless $ip = &valid_ip($ip);
-	warn "nodesave: no node data for $ip!" and return
-		unless defined $node;
-	print "saving $node->{ip}:$node->{port} $node->{ts} epoch s\n" if $opt{v};
-	if (open NODE, "> $nodedir/$ip.php") {
-		flock NODE,LOCK_SH;
-		print NODE serialize($node);
-		flock NODE,LOCK_UN;
-		close NODE;
-		return 1;
-	}
-	warn "error opening $nodedir/$ip.php: $!";
-	0;
 }
 
 sub parse_req {
@@ -196,7 +146,7 @@ sub parse_req {
 	my ($cmd,$data) = ($buff=~/^(\w+)(.*)/);
 	$data =~ /^\s*/; 
 	$data =~ /\s*$/s;
-	return unless defined $reqtype{$cmd};
+	return unless defined $napreq{$cmd};
 	my $req = {
 		cmd => $cmd,
 		data => $data,
@@ -211,102 +161,5 @@ sub parse_req {
 sub do_req {
 	my ($req) = @_;
 	print "processing $req->{cmd}\n" if $opt{v};
-	return &{$reqtype{$req->{cmd}}}($req);
-}
-######################## callbacks #####################################
-# list of currently alive ips
-sub peers {
-	my ($req) = @_;
-	my $sock = $req->{sock};
-	foreach my $ipfile (glob "$nodedir/[0-9]*.[0-9]*.[0-9]*.[0-9]*.php") {
-		my $ip = valid_ip($ipfile);
-		my $node = &nodeload($ip);
-		next unless $node;
-		# if we haven't heard from someone assume they are no longer with us
-		unlink $node->{fifo} and next if time - $node->{ts} > $nodelife;
-		print "$ip $node->{desc}\n" if $opt{v};
-		print $sock "$ip $node->{desc}\n";
-	}
-	print $sock "END\n";
-	1;
-}
-# needs a neighbour ip
-sub getindex {
-	my ($req) = @_;
-	my ($theirip) = ($req->{data} =~ m#(\S+)#);
-	my $node = nodeload($theirip);
-	warn "getindex: no node for $theirip!" and return unless defined $node;
-	warn "no peer socket for $theirip!" and return unless -S $node->{fifo};
-	my $fifoconn = IO::Socket::UNIX->new(
-		Peer => $node->{fifo},
-		Type => SOCK_STREAM,
-		Timeout => 2,
-	) or warn "getindex: can't open socket: $!";
-	my $sel = IO::Select->new;
-	$sel->add($fifoconn);
-	print $fifoconn "index\n";
-	$fifoconn->flush;
-	my @readers = $sel->can_read;
-	foreach my $reader (@readers) {
-		next unless $reader == $fifoconn;
-		my $reqsock = $req->{sock};
-		while (my $data = <$fifoconn>) {
-			print $reqsock $data;
-		}
-	}
-	close $fifoconn;
-	1;
-}
-
-# needs neighbour ip and file id
-sub getfile {
-	my ($req) = @_;
-	1;
-}
-# sends continuous stream of bytes
-# needs a neighbour ip
-sub stream {
-	my ($req) = @_;
-	1;
-}
-__END__
-Monday, September 19, 2005
-Unix Domain Sockets in Perl
-
-I know this has been done dozens of times before, but it's been a real headache for me lately. A full example of how to use IO::Socket::UNIX is not really available anywhere on the web. All of the socket info for perl seems to assume you've been doing socket programming in C for years, and theres very limited info on doing Unix Domain Sockets. it all seems to be about inet sockets. which, of course, relates a lot to Unix Sockets, but there are some differences.
-
-okay, here is my working example. Its meant to be run twice, the first time it's the server, the second time it's the client.
-
-
-
-#!/usr/bin/perl
-use strict; $|++;
-use IO::Socket;
-
-my $socketfile = $ENV{HOME} . "/.sockettest";
-
-if ( -S $socketfile ) {
- # client!
- my $client = IO::Socket::UNIX->new(Peer  => $socketfile,
-                                    Type      => SOCK_STREAM ) or die $!;
- my $string = "this is some sent garbage.\n";
- print $client $string;
- $client->flush;
- $client->close;
- exit;
-
-} else {
- # server!
- unlink $socketfile;
- my $data;
- my $server = IO::Socket::UNIX->new(Local => $socketfile,
-                                    Type      => SOCK_STREAM,
-                                    Listen    => 32 ) or die $!;
- $server->autoflush(1);
- while ( my $connection = $server->accept() ) {
-   my $data= <$connection>;
-   print $data, $/;
-
-   sleep 5;
- }
+	return &{$napreq{$req->{cmd}}}($req);
 }
---
+++ napmedia/naptime/napclient.pl
@@ -0,0 +1,119 @@
+#!/usr/bin/perl
+# purpose of this script is to implement a simple nap client
+# probably this would be a shell script and a c program on the EM2440 board 
+# idea is to send commands to this client via a unix socket and wait for a response
+use Nap qw/$naphost %napreq $napport $napnodelife/;
+use IO::Socket::INET;
+use IO::Socket::UNIX;
+use IO::Select;
+use strict;
+
+my $sockfile = "napclient.sock";
+
+my $sel = IO::Select->new;
+my $remotesock;
+my $localsock;
+my %napresponse = (
+	'index' => \&index_response,
+	'getfile' => \&getfile_response,
+);
+sub index_response {
+	my ($sock, $data) = @_;
+	return &getfile_response($sock, "index.txt");
+}
+sub getfile_response {
+	my ($sock, $data) = @_;
+	# remove any .././s at start of file
+	my $file = ($data =~ m#^[\s\./]*(.*)#)[0];
+	print $sock "FINDFAIL $file\n" and return unless -f $file;
+	open FILE, $file or print "OPENFAIL $file\n" and return;
+	my $fsize = (stat($file))[7];
+	print $sock "RESPONSE $fsize $file\n\n";
+	while (<FILE>) {
+		print $sock;
+	}
+	close FILE;
+	return;
+}
+
+$SIG{INT} = \&cleanup;
+
+while (1) {
+	# our local communication channel
+	unless (defined $localsock and $localsock->connected) {
+		$sel->remove($localsock) if defined $localsock;
+		warn "$sockfile still alive - unlinking (is there another client running?)" 
+			and unlink $sockfile if -S $sockfile;
+		$localsock = IO::Socket::UNIX->new(
+			Type => SOCK_STREAM,
+			Local => $sockfile,
+			Listen => 32,
+		) or die "socket error: $!"; # actually need local communications to work at all
+		print "server listening on $sockfile\n";
+		$localsock->autoflush(1);
+		$sel->add($localsock);
+	}
+
+	# try and open a connection with the server
+	unless (defined $remotesock and $remotesock->connected) {
+		$sel->remove($remotesock) if defined $remotesock;
+		$remotesock = IO::Socket::INET->new(
+			PeerHost => "$naphost:$napport",
+			Proto => 'tcp',
+		) or warn "can't connect to $naphost:$napport: $!" and next;
+		print "established connection to $naphost:$napport\n";
+		$remotesock->autoflush(1);
+		$sel->add($remotesock);
+	}
+
+	print "waiting for something ...\n";
+	my $localconn;
+	while (my @readers = $sel->can_read) {
+		print "got something\n";
+		foreach my $reader (@readers) {
+			if ($reader == $localsock) {
+				print "from local socket\n";
+				$localconn = $localsock->accept;
+				my $data = <$localconn>;
+				print $remotesock $data;
+				$remotesock->flush;
+				print $data;
+
+			} elsif ($reader == $remotesock) {
+				print "from remote socket\n";
+				if (defined $localconn and $localconn->connected) {
+					my $response;
+					$response = <$remotesock>;
+					if ($response =~ /^RESPONSE (\d+)/) {
+						my $bytes = $1;
+						<$remotesock>;
+						$remotesock->recv($remotesock, $response, $bytes);
+					}
+					while (($response .=  <$remotesock>) !~ /^END/m) {}
+					print "forwarding response\n$response";
+					print $localconn $response; 
+					$localconn->flush;
+					$localconn->close;
+				} else {
+					# in this case we've been asked to do something by a remote peer
+					my $request = <$remotesock>;
+					print "request: $request";
+					my ($cmd, $cmddata) = ($request =~ /(\S*)(.*)/);
+					if (defined $napresponse{$cmd}) {
+						&{$napresponse{$cmd}}($remotesock, $cmddata);
+						$remotesock->flush;
+					}
+				}
+			}
+			sleep 1;
+		}
+	}
+	sleep 5;
+}
+
+sub cleanup {
+	$remotesock->close() if defined $remotesock and $remotesock->connected;
+	$localsock->close() if defined $localsock and $localsock->connected;
+	unlink $sockfile;
+	exit;
+}
