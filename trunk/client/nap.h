03ba7a47f7de88eb6484d36c5e108cc75a42b131
cwoodruf@gmail.com <cwoodruf@gmail.com@b7c43c20-ba61-c9b8-7786-de23efc508d3>
2011-11-08 18:12:12 +0000

    forwarding commands to bridge not working yet, generate_response not implemented yet

--- trunk/client/nap.h
+++ trunk/client/nap.h
@@ -2,7 +2,7 @@
 #define NAP
 
 /* maximum size that can be transfered atomically on a unix domain socket */
-#define BUFFSIZE 	4096
+#define NAPBUFFSIZE 	4096
 /* known bug that this is not defined in sys/un.h */
 #define UNIX_PATH_MAX   108
 
@@ -16,5 +16,7 @@
 
 #define NAPCOMMAND_VALIDATE 		"validate"
 #define NAPRESPONSE_VALID 		"VALID"
+/* generic response should be followed by an integer byte value */
+#define NAPRESPONSE			"RESPONSE"
 
 #endif /* NAP */
