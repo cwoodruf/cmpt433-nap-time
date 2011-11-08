2be677235b405720ee7219c9c7a0869dc1bc0e4d
cwoodruf@gmail.com <cwoodruf@gmail.com@b7c43c20-ba61-c9b8-7786-de23efc508d3>
2011-11-08 17:12:15 +0000

    restored napwrite.pl

--- trunk/napmedia/naptime/napwrite.pl
+++ trunk/napmedia/naptime/napwrite.pl
@@ -1,1 +1,39 @@
-peers ../../napclient.soc
+#!/usr/bin/perl
+use IO::Socket::UNIX;
+use Data::Dumper;
+use File::Path;
+use strict;
+my $client;
+$SIG{INT} = sub { $client->close; exit; };
+
+while (<STDIN>) {
+	$client = IO::Socket::UNIX->new(
+		Peer => "$ENV{HOME}/napclient.sock",
+		Type => SOCK_STREAM,
+	) or die $!;
+
+	print $client $_;
+	$client->flush;
+	my $response = <$client>;
+	print "$response";
+	if ($response =~ /^RESPONSE (\d+) +(\S.*)/) {
+		my $bytes = $1;
+		my $fname = $2;
+		my $dir = ($fname=~m#(.*)/#)[0];
+		mkpath $dir unless $dir eq '' or -d $dir;
+		open FILE, "> $fname" or die "can't open file: $!";
+		<$client>;
+		my $cbuff;
+		while (read($client,$cbuff,$bytes)) {
+			print FILE $cbuff;
+		}
+		close FILE;
+		print Dumper([stat($fname)]);
+	} else {
+		while (<$client>) {
+			print $_;
+		}
+	}
+	$client->close;
+}
+
