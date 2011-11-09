66e29709469d6b336a8d016eb63282106b9e6456
cwoodruf@gmail.com <cwoodruf@gmail.com@b7c43c20-ba61-c9b8-7786-de23efc508d3>
2011-11-09 16:45:57 +0000

    adding basic utilities for managing shared indexes

---
+++ p2p/indexline.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sha=`sha1sum "$1" | sed -e 's/^\([a-f0-9]*\).*/\1/`
+size=`stat -c"%s" "$1"`
+if [ $2 ] ; 
+then
+	file=`sed -e 's#^$2##' "$1"`
+else 
+	file=$1
+fi
+echo $sha $size $file
