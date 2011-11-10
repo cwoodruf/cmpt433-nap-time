a395014283983738048087661cce4a2c7e71cdfd
cwoodruf@gmail.com <cwoodruf@gmail.com@b7c43c20-ba61-c9b8-7786-de23efc508d3>
2011-11-10 08:12:25 +0000

    added rudimentary multicast app

---
+++ trunk/napscan/napmulticast.h
@@ -0,0 +1,16 @@
+/* 
+ * 
+ * adapted from
+ * sender.c -- multicasts "hello, world!" to a multicast group once a second
+ * http://ntrg.cs.tcd.ie/undergrad/4ba2/multicast/antony/example.html
+ * Antony Courtney,	25/11/94
+ */
+
+#ifndef NAPMULTICAST
+#define NAPMULTICAST
+
+#define NAP_PORT 22534
+#define NAP_GROUP "225.0.0.34"
+
+#endif /* NAPMULTICAST */
+
