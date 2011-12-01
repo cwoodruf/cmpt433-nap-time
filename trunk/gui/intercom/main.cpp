dbc6aefe174545e8e56c8c80ec8d41a50017e9bd
cwoodruf@gmail.com <cwoodruf@gmail.com@b7c43c20-ba61-c9b8-7786-de23efc508d3>
2011-12-01 05:34:26 +0000

    initial frame for intercom application

---
+++ trunk/gui/intercom/main.cpp
@@ -0,0 +1,17 @@
+/**
+ * cmpt433 nap time systems Cal Woodruff <cwoodruf@sfu.ca> 301013983 
+ */
+#include <QtGui/QApplication>
+#include "intercom.h"
+
+int main(int argc, char *argv[])
+{
+	QApplication a(argc, argv);
+	MainWindow w;
+
+	if (IS_EM2440) w.showMaximized();
+	else w.showNormal();
+
+	return a.exec();
+}
+
