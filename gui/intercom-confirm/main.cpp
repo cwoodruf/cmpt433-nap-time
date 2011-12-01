fdedf8476ac0ef4b1926423fd182d572e5bd11a1
cwoodruf@gmail.com <cwoodruf@gmail.com@b7c43c20-ba61-c9b8-7786-de23efc508d3>
2011-12-01 07:16:59 +0000

    making full window for intercom-confirm

--- gui/intercom-confirm/main.cpp
+++ gui/intercom-confirm/main.cpp
@@ -1,32 +1,17 @@
 /**
  * cmpt433 nap time systems Cal Woodruff <cwoodruf@sfu.ca> 301013983 
- * this should pop up a question window and alert an end user that an
- * intercom connection is being requested
  */
 #include <QtGui/QApplication>
-#include <QMessageBox>
+#include "intercom-confirm.h"
 
 int main(int argc, char *argv[])
 {
-	QString name, host, ip;
-	int res;
-
 	QApplication a(argc, argv);
-	if (argc < 4) {
-		printf("error: missing arguments should have name host and ip");
-	} else {
-		name = QString(argv[1]);
-		host = QString(argv[2]);
-		ip = QString(argv[3]);
+	MainWindow w;
 
-		res = QMessageBox::question(&a, "Connection Request", 
-			name+"("+host+"/"+ip+") wants to speak to you. Click Ok to accept."
-			QMessageBox::Cancel|QMessageBox::Ok);
-		if (res == QMessageBox::Ok) {
-			printf("OK");
-		}
-	}
-	printf("connection refused");
+	if (IS_EM2440) w.showMaximized();
+	else w.showNormal();
+
 	return a.exec();
 }
 
