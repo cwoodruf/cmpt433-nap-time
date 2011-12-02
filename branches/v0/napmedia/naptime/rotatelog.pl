24ff6bee1a2b6ed525128897ecf1ac9b76ffbc2d
cwoodruf@gmail.com <cwoodruf@gmail.com@b7c43c20-ba61-c9b8-7786-de23efc508d3>
2011-12-02 19:57:53 +0000

    removed code not used in end product

--- branches/v0/napmedia/naptime/rotatelog.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-# rotate the napmedia logfile if it gets too big
-$naplog = shift || '/usr/local/apache2/htdocs/napmedia/napbridge.txt';
-$maxlog = shift || 1024*100;
-$stat = stat($naplog);
-exit if $stat[7] < $maxlog;
-rename $naplog, "$naplog~";
-system "tail > '$naplog' '$naplog~'";
-unlink "$naplog~";
--- branches/v0/napmedia/views/templates/nodeedit.tpl
+++
@@ -1,3 +0,0 @@
-<a href="?action=node">Back to Node setup</a>
-<h3>Node {$ip}</h3>
-{var_export var=$all}
