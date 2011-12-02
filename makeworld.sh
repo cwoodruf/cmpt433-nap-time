641fe50930ebf4d87906118cd95e12fdb0e9ecfc
cwoodruf@gmail.com <cwoodruf@gmail.com@b7c43c20-ba61-c9b8-7786-de23efc508d3>
2011-12-02 21:13:31 +0000

    some last minute tweaks and a README

--- makeworld.sh
+++ makeworld.sh
@@ -6,6 +6,21 @@
 # see our how to guide (separate) for instructions on how to make these components
 # you will want to change the LIB defined in the Makefile to point where these reside
 # they are assumed to all be in the same subdirectory
+usage="Usage: $0 {directory with alsa ortp and speex}"
 
-make -B 
+if [ "$PUBLIC" = "" ]
+then
+	echo You need to define a shell variable PUBLIC to a directory for the output of the make
+	echo $usage
+	return
+fi
+if [ "$1" = "" ] || [ ! -d "$1/alsa" ] || [ ! -d "$1/ortp" ] 
+then
+	echo You need our precompiled libraries to build the naprtp component
+	echo $usage
+	echo to run anyway run make -B instead of this script
+	return
+fi
+
+make -B LIB="$1"
 
