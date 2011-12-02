4c4cee143a1ebe4c59b19ac57064698e02c35950
bestbetz@gmail.com <bestbetz@gmail.com@b7c43c20-ba61-c9b8-7786-de23efc508d3>
2011-12-02 11:10:55 +0000

    minor corrections to gui

--- trunk/gui/player/ui_gui_MainPlay.h
+++ trunk/gui/player/ui_gui_MainPlay.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'gui_MainPlay.ui'
 **
-** Created: Thu Dec 1 22:12:25 2011
+** Created: Fri Dec 2 03:02:48 2011
 **      by: Qt User Interface Compiler version 4.7.4
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
@@ -15,7 +15,6 @@
 #include <QtGui/QApplication>
 #include <QtGui/QButtonGroup>
 #include <QtGui/QCommandLinkButton>
-#include <QtGui/QDial>
 #include <QtGui/QHeaderView>
 #include <QtGui/QLabel>
 #include <QtGui/QListWidget>
@@ -33,11 +32,7 @@ public:
     QWidget *centralwidget;
     QLabel *label;
     QCommandLinkButton *allSongsButton;
-    QLabel *label_2;
-    QLabel *label_3;
-    QLabel *label_4;
     QListWidget *listWidget;
-    QDial *dial;
     QPushButton *playButton;
     QPushButton *prevButton;
     QPushButton *pauseButton;
@@ -55,48 +50,32 @@ public:
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
         label = new QLabel(centralwidget);
         label->setObjectName(QString::fromUtf8("label"));
-        label->setGeometry(QRect(9, 9, 181, 17));
+        label->setGeometry(QRect(9, 10, 181, 17));
         allSongsButton = new QCommandLinkButton(centralwidget);
         allSongsButton->setObjectName(QString::fromUtf8("allSongsButton"));
-        allSongsButton->setGeometry(QRect(200, 0, 121, 29));
-        label_2 = new QLabel(centralwidget);
-        label_2->setObjectName(QString::fromUtf8("label_2"));
-        label_2->setGeometry(QRect(250, 130, 67, 17));
-        label_3 = new QLabel(centralwidget);
-        label_3->setObjectName(QString::fromUtf8("label_3"));
-        label_3->setGeometry(QRect(242, 100, 16, 21));
-        label_4 = new QLabel(centralwidget);
-        label_4->setObjectName(QString::fromUtf8("label_4"));
-        label_4->setGeometry(QRect(296, 100, 21, 21));
+        allSongsButton->setGeometry(QRect(150, 0, 151, 29));
         listWidget = new QListWidget(centralwidget);
         listWidget->setObjectName(QString::fromUtf8("listWidget"));
-        listWidget->setGeometry(QRect(11, 31, 221, 111));
-        dial = new QDial(centralwidget);
-        dial->setObjectName(QString::fromUtf8("dial"));
-        dial->setGeometry(QRect(240, 50, 71, 71));
+        listWidget->setGeometry(QRect(11, 31, 291, 128));
         playButton = new QPushButton(centralwidget);
         playButton->setObjectName(QString::fromUtf8("playButton"));
-        playButton->setGeometry(QRect(11, 151, 41, 27));
+        playButton->setGeometry(QRect(11, 160, 41, 27));
         prevButton = new QPushButton(centralwidget);
         prevButton->setObjectName(QString::fromUtf8("prevButton"));
-        prevButton->setGeometry(QRect(120, 151, 81, 27));
+        prevButton->setGeometry(QRect(120, 160, 81, 27));
         pauseButton = new QPushButton(centralwidget);
         pauseButton->setObjectName(QString::fromUtf8("pauseButton"));
-        pauseButton->setGeometry(QRect(60, 151, 51, 27));
+        pauseButton->setGeometry(QRect(60, 160, 51, 27));
         nextButton = new QPushButton(centralwidget);
         nextButton->setObjectName(QString::fromUtf8("nextButton"));
-        nextButton->setGeometry(QRect(210, 151, 85, 27));
+        nextButton->setGeometry(QRect(210, 160, 85, 27));
         PlayerWindow->setCentralWidget(centralwidget);
         playButton->raise();
         prevButton->raise();
         nextButton->raise();
         label->raise();
         allSongsButton->raise();
-        label_2->raise();
-        label_3->raise();
-        label_4->raise();
         listWidget->raise();
-        dial->raise();
         pauseButton->raise();
         statusbar = new QStatusBar(PlayerWindow);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
@@ -113,12 +92,9 @@ public:
         actionGreat_Music->setText(QApplication::translate("PlayerWindow", "Great Music", 0, QApplication::UnicodeUTF8));
         label->setText(QApplication::translate("PlayerWindow", "Playlist Songs", 0, QApplication::UnicodeUTF8));
         allSongsButton->setText(QApplication::translate("PlayerWindow", "List All Songs", 0, QApplication::UnicodeUTF8));
-        label_2->setText(QApplication::translate("PlayerWindow", "Volume", 0, QApplication::UnicodeUTF8));
-        label_3->setText(QApplication::translate("PlayerWindow", "0", 0, QApplication::UnicodeUTF8));
-        label_4->setText(QApplication::translate("PlayerWindow", "10", 0, QApplication::UnicodeUTF8));
         playButton->setText(QApplication::translate("PlayerWindow", "Play", 0, QApplication::UnicodeUTF8));
         prevButton->setText(QApplication::translate("PlayerWindow", "Previous", 0, QApplication::UnicodeUTF8));
-        pauseButton->setText(QApplication::translate("PlayerWindow", "Pause", 0, QApplication::UnicodeUTF8));
+        pauseButton->setText(QApplication::translate("PlayerWindow", "Stop", 0, QApplication::UnicodeUTF8));
         nextButton->setText(QApplication::translate("PlayerWindow", "Next", 0, QApplication::UnicodeUTF8));
     } // retranslateUi
 
